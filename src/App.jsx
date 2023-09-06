263faa81bf02ae95386253565e410c263441d3c4
Rezoanul hasan <rezoanulhasan96@gmail.com>
2023-09-06 10:17:14 +0600

    add navbar and fotter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom'
+import Footer from './Component/Shared/Footer/Footer';
 const App = () => {
   return (
     <div>
  <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
-   
+   <Footer></Footer>
     </div>
   );
 };
