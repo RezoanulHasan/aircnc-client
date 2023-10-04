f59a203d8b34ac5adce7f71688849a90d59f2cfc
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-10-04 07:20:45 +0600

    add show more button

--- src/Component/Pages/Rooms/Rooms.jsx
+++ src/Component/Pages/Rooms/Rooms.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from '../../Shared/Container';
@@ -10,7 +11,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import GetRandomColor from '../../../Hooks/GetRandomColor';
 import { Fade } from "react-awesome-reveal";
 
-
 const Rooms = () => {
   const [params, setParams] = useSearchParams();
   const category = params.get('category');
@@ -20,12 +20,10 @@ const Rooms = () => {
   const [priceRange, setPriceRange] = useState('');
   const [propertyType, setPropertyType] = useState('');
   const [bedroom, setBedroom] = useState('');
-  const [guests, setGuests ]= useState('');
-  const [bathroom, setBathroom ]= useState('');
+  const [guests, setGuests] = useState('');
+  const [bathroom, setBathroom] = useState('');
   const [regions, setRegions] = useState('');
-
-
- 
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
     fetch('https://aircnc-server-k3xjzddn8-rezoanulhasan.vercel.app/rooms')
@@ -37,7 +35,6 @@ const Rooms = () => {
         } else {
           setRooms(data);
         }
-
         setLoading(false);
       })
       .catch((err) => {
@@ -59,26 +56,24 @@ const Rooms = () => {
       if (!propertyType) return true;
       return room.property.toLowerCase() === propertyType.toLowerCase();
     })
-
-   .filter((room) => {
-    if (!regions) return true;
-    return room.region.toLowerCase() === regions.toLowerCase();
-  })
+    .filter((room) => {
+      if (!regions) return true;
+      return room.region.toLowerCase() === regions.toLowerCase();
+    })
     .filter((room) => {
       if (!bedroom) return true;
       return room.bedroom === parseInt(bedroom, 10);
-    }) // Apply the bedroom filter
+    })
     .filter((room) => {
       if (!guests) return true;
       return room.guests === parseInt(guests, 10);
-    }) // Apply the bed filter
-
+    })
     .filter((room) => {
       if (!bathroom) return true;
       return room.bathroom === parseInt(bathroom, 10);
-    }); // Apply the    bathroom filter
-  
-    
+    });
+
+  const displayedRooms = showAll ? filteredRooms : filteredRooms.slice(0, 20);
 
 
 
@@ -217,24 +212,47 @@ const Rooms = () => {
       </Fade>
       {loading ? (
         <Spinner />
-      ) : filteredRooms.length > 0 ? (
-        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {filteredRooms.map((room) => (
-            <ShowRoomData key={room._id} room={room} />
-          ))}
-        </div>
-      ) : (
-        <div className='pt-12'>
-          <Heading
-            title='No Rooms Available In This Category!'
-            subtitle='Please Select Other Categories.'
-            center={true}
-          />
-        </div>
-      )}
-    </Container>
-  );
-};
+        ) : displayedRooms.length > 0 ? (
+          <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+            {displayedRooms.map((room) => (
+              <ShowRoomData key={room._id} room={room} />
+            ))}
+          </div>
+        ) : (
+          <div className='pt-12'>
+            <Heading
+              title='No Rooms Available In This Category!'
+              subtitle='Please Select Other Categories.'
+              center={true}
+            />
+          </div>
+        )}
+        {filteredRooms.length > 20 && !showAll && (
+          <div className='flex justify-center mt-10'>
+            <button
+              onClick={() => setShowAll(true)}
+              className='flex items-center bg-rose-500 text-white px-20  py-5 rounded-full shadow-md hover:shadow-lg transition'
+            >
+              <span>Show All</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 ml-2"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        )}
+      </Container>
+    );
+  };
 
 const ShowRoomData = ({ room }) => {
   const textColor = GetRandomColor();
